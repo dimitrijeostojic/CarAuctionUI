05dc3ea721858457811b77e7179fa823f2577090
Dimitrije <dimitrije219@gmail.com>
2025-06-28 17:25:58 +0200

    Added login functionality

---
+++ auth.ts
@@ -0,0 +1,31 @@
+import NextAuth, { Profile } from "next-auth";
+import { OIDCConfig } from "next-auth/providers";
+import DuendeIdentityServer6 from "next-auth/providers/duende-identity-server6";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [
+        DuendeIdentityServer6({
+            id: "id-server",
+            clientId: "nextApp",
+            clientSecret: "secret",
+            issuer: "http://localhost:5001",
+            authorization: { params: { scope: "openid profile auctionApp" } },
+            idToken: true,
+        } as OIDCConfig<Omit<Profile,'username'>>)
+    ],
+    callbacks: {
+        async jwt({ token, profile }) {
+            if (profile) {
+                token.username = profile.username
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user.username = token.username
+            }
+            return session;
+        }
+    }
+
+})
